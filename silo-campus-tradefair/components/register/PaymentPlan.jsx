77eadebbf432073a70b57f6c3069375e36c17b48
Obi Kennedy <166854306+ObiKennedi@users.noreply.github.com>
2026-04-23 01:53:37 +0100

    Fix english

--- silo-campus-tradefair/components/register/PaymentPlan.jsx
+++ silo-campus-tradefair/components/register/PaymentPlan.jsx
@@ -5,9 +5,9 @@ const PaymentPlan = () => {
         <div className="payment-plan">
             <h1>Payment Plan</h1>
             <div className="payment-grid">
-                <p>1 Campus: ₦70,000</p>
-                <p>2 Campuses: ₦130,000</p>
-                <p>3 Campuses: ₦180,000</p>
+                <p>Payment for 1 campus: ₦70,000</p>
+                <p>Payment for 2 campuses: ₦130,000</p>
+                <p>Payment for 3 campuses: ₦180,000</p>
             </div>
         </div>
     );
