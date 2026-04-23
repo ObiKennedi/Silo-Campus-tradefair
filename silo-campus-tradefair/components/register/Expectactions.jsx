56cd816ca5ec4e54fd1f9c37ed7aad923a35507a
Obi Kennedy <166854306+ObiKennedi@users.noreply.github.com>
2026-04-23 01:57:59 +0100

    Fix expectations

--- silo-campus-tradefair/components/register/Expectactions.jsx
+++ silo-campus-tradefair/components/register/Expectactions.jsx
@@ -1,25 +1,20 @@
-import { FaWhatsapp } from "react-icons/fa";
-import "../styles/WhatsAppFloat.scss";
+import "../../styles/registeration/Expectactions.scss";
 
-const WhatsappFloat = () => {
-    const phone = "2349063508366";
-
-    const message =
-        "Hello, I’d like to make an enquiry about Silo Campus Tradefair.";
-
-    const link = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-
+const Expectactions = () => {
     return (
-        <a
-            href={link}
-            className="whatsapp-float"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <span className="whatsapp-text">Chat with us</span>
-            <FaWhatsapp />
-        </a>
+        <section className="expectations">
+            <h1>Benefits of Registering</h1>
+            <p>By registering for the 2026 Silo Campus Trade Fair, you are entitled to the following:</p>
+            <ul>
+                <li>Access to exhibit at any of the tradefairs you registered for</li>
+                <li>5 by 10 feet exhibition space under the tent</li>
+                <li>One table and one chair for exhibition</li>
+                <li>A vendors confirmation flier for the event</li>
+                <li>Branded souvenirs</li>
+            </ul>
+            <small>Note: Due to to the current weather condition, we will be using a tent for the exhibition, to prevent being interrupted by storms.</small>
+        </section>
     );
 };
 
-export default WhatsappFloat;
+export default Expectactions;
